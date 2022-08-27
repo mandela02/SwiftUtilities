d81dde615721bf2ee8445bbfa4490b5f7b0c4ff1
TriBQ <10khongquang@gmail.com>
2022-08-27 21:24:03 +0700

    add more extension

--- Sources/IosUtilities/Views/Base/BaseHostingViewController.swift
+++ Sources/IosUtilities/Views/Base/BaseHostingViewController.swift
@@ -8,14 +8,14 @@
 import Foundation
 import SwiftUI
 
-public class BaseHostingViewController<Content>: UIHostingController<AnyView> where Content: View {
+open class BaseHostingViewController<Content>: UIHostingController<AnyView> where Content: View {
     public init(shouldShowNavigationBar: Bool = false,
                 rootView: Content) {
         super.init(rootView: AnyView(rootView
             .navigationBarHidden(!shouldShowNavigationBar)))
     }
     
-    @objc required dynamic init?(coder aDecoder: NSCoder) {
+    @objc required dynamic public init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
