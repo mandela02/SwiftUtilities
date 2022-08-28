f6c10cc2688e1e523c24e2807d648ad891b9c914
TriBQ <10khongquang@gmail.com>
2022-08-28 15:04:21 +0700

    add more extension

---
+++ Sources/IosUtilities/Model/LoadingStatus.swift
@@ -0,0 +1,15 @@
+//
+//  File.swift
+//  
+//
+//  Created by TriBQ on 28/08/2022.
+//
+
+import Foundation
+
+public enum LoadingStatus {
+    case initial
+    case inProcess
+    case success
+    case error(String)
+}
