b960e05fdfeef5091477e36ad1d0c863a35a86e3
Tri Bui Q. VN.Hanoi <tri.bui@monstar-lab.com>
2023-02-17 14:18:11 +0700

    remeve unused code

--- Sources/IosUtilities/Utilities/Extension/Notification+Extension.swift
+++ Sources/IosUtilities/Utilities/Extension/Notification+Extension.swift
@@ -6,8 +6,3 @@
 //
 
 import Foundation
-
-public extension Notification.Name {
-    static let themeDidChange = Notification.Name("THEME_DID_CHANGE")
-    static let languageDidChange = Notification.Name("LANGUAGE_DID_CHANGE")
-}
